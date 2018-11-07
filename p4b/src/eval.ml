accac807c1e9d69a7665210ffe1254c3c867752e
Cameron Moy <mail@camoy.name>
2018-11-07 01:10:56 -0500

    Add p4b

---
+++ p4b/src/eval.ml
@@ -0,0 +1,10 @@
+open SmallCTypes
+open EvalUtils
+
+exception TypeError of string
+exception DeclareError of string
+exception DivByZeroError
+
+let rec eval_expr env e = failwith "unimplemented"
+
+let rec eval_stmt env s = failwith "unimplemented"
